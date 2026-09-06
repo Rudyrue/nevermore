276c008e7d031ccfd5f31136ea26870128381a0b
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2026-09-06 14:51:25 -0700

    etterna quants

--- src/nevermore/core/Quantization.hx
+++ src/nevermore/core/Quantization.hx
@@ -2,10 +2,18 @@ package nevermore.core;
 
 import nevermore.core.timing.TimingMap;
 
+// these games don't necessarily *have* a set quant list
+// as quants depend on the skin, rather than the game setting them (like in this scenario)
+// the quant list you see here is dependent on the game's default noteskin
+// sm being "default" (no shit)
+// itg being metal/cel
+// etterna being the byzero series
+// etc
 #if !NEVERMORE_NO_QUANTIZATION
 enum abstract QuantType(String) from String to String {
 	var STEPMANIA = 'StepMania';
 	var ITG = 'In The Groove';
+	var ETTERNA = 'Etterna';
 	var ARROWVORTEX = 'ArrowVortex';
 	var CUSTOM = 'Custom';
 }
@@ -40,18 +48,32 @@ class Quantization {
 			GRAY     // 192nd
 		],
 
+		'Etterna' => [
+			RED,     // 4th
+			BLUE,    // 8th
+			GREEN,   // 12th
+			YELLOW,  // 16th
+			GRAY,    // 20th
+			MAGENTA, // 24th
+			ORANGE,  // 32nd
+			TEAL,    // 48th
+			GRAY,    // 64th
+			GRAY,    // 96th
+			GRAY     // 192nd
+		],
+
 		'In The Groove' => [
-			RED,       // 4th
-			BLUE,      // 8th
-			PURPLE,    // 12th
-			GREEN,     // 16th
-			TEAL, // 20th 
-			TEAL, // 24th
-			ORANGE,    // 32nd
-			TEAL, // 48th
-			TEAL, // 64th
-			TEAL, // 96th
-			TEAL  // 192nd
+			RED,    // 4th
+			BLUE,   // 8th
+			PURPLE, // 12th
+			GREEN,  // 16th
+			TEAL,   // 20th 
+			TEAL,   // 24th
+			ORANGE, // 32nd
+			TEAL,   // 48th
+			TEAL,   // 64th
+			TEAL,   // 96th
+			TEAL    // 192nd
 		],
 
 		'ArrowVortex' => [
